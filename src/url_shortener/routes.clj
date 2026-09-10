fdcef25647c6eb0782fc59feccf7a26dabd442ca
NinaA2215 <atrajic2215@gmail.com>
2026-09-11 00:55:14 +0200

    Added dependency injection at the routing layer

--- src/url_shortener/routes.clj
+++ src/url_shortener/routes.clj
@@ -1,16 +1,26 @@
 (ns url-shortener.routes
   (:require [reitit.ring :as ring]
             [url-shortener.handlers :as h]
+            [url-shortener.db :as db]
             [ring.middleware.params :refer [wrap-params]]))
 
 (def app
-  (wrap-params
     (ring/ring-handler
       (ring/router
-        [["/api/shorten" {:post h/shorten-handler}]
-         ["/:code" {:get h/redirect-handler}]
-         ["/" {:get h/html-handler}]
-         ["/form/shorten" {:post h/html-shorten-handler}]
-         ])
-      (ring/create-default-handler)))
+        [["/api/shorten"
+          {:post (fn [request]
+                   (h/shorten-handler db/ds request))}]
+
+         ["/:code"
+          {:get (fn [request]
+                  (h/redirect-handler db/ds request))}]
+
+         ["/"
+          {:get h/html-handler}]
+
+         ["/form/shorten"
+          {:post (fn [request]
+                   (h/html-shorten-handler db/ds request))
+           :middleware [wrap-params]}]])
+      (ring/create-default-handler))
   )
