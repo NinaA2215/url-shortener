5bc625316e6e58c9f4e2b00352424ea01702b1fb
NinaA2215 <atrajic2215@gmail.com>
2026-09-11 04:38:44 +0200

    Fixed manual HTML construction and formatting

--- src/url_shortener/handlers.clj
+++ src/url_shortener/handlers.clj
@@ -1,14 +1,15 @@
 (ns url-shortener.handlers
   (:require [clojure.string :as str]
             [url-shortener.db :as db]
-            [cheshire.core :as json])
+            [cheshire.core :as json]
+            [hiccup2.core :as h])
   (:import (com.fasterxml.jackson.core JsonParseException)
            (java.net MalformedURLException URL)))
 
-
 (def base-url
   (or (System/getenv "BASE_URL")
-                     "http://localhost:3000/"))
+      "http://localhost:3000/"))
+
 (def letters "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 (def short-code-length 6)
 (def max-code-generation-attempts 5)
@@ -18,20 +19,24 @@
 (defn html-handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<form method=\"post\" action=\"/form/shorten\"><input type=\"text\" name=\"url\"><button type=\"submit\">Shorten</button></form>"}
-  )
+   :body (str (h/html
+                [:form {:method "post" :action "/form/shorten"}
+                 [:input {:type "text" :name "url"}]
+                 [:button {:type "submit"} "Shorten"]]))})
 
 (defn random-shortener []
-    (apply str (repeatedly short-code-length #(rand-nth letters))))
+  (apply str (repeatedly short-code-length #(rand-nth letters))))
 
 (defn generate-unique-code! [ds url]
   (loop [code (random-shortener)
          attempts 1]
     (if (> attempts max-code-generation-attempts)
-      (throw (RuntimeException. "Unable to generate unique short code."))
-      (let [result(db/insert-link! ds code url)]
+      (throw (RuntimeException.
+               "Unable to generate unique short code."))
+      (let [result (db/insert-link! ds code url)]
         (if (= result :collision)
-          (recur (random-shortener)(inc attempts))
+          (recur (random-shortener)
+                 (inc attempts))
           code)))))
 
 (defn shorten-url! [ds url]
@@ -39,7 +44,6 @@
     {:code code
      :short-url (str base-url code)}))
 
-;; Checking if passed url is blank and if it has valid protocol
 (defn valid-url? [^String url]
   (try
     (let [address (URL. url)
@@ -61,7 +65,8 @@
         (cond
           (= read-bytes -1)
           (.toString output "UTF-8")
-          (> (+ (.size output) read-bytes) max-request-body-size)
+          (> (+ (.size output) read-bytes)
+             max-request-body-size)
           (throw (IllegalArgumentException. "Request body too large."))
           :else
           (do
@@ -71,48 +76,57 @@
 (defn shorten-handler [ds request]
   (let [body (try
                (json/parse-string (read-request-body request) true)
-               (catch IllegalArgumentException _
-                 ::body-too-large)
                (catch JsonParseException _
-                ::invalid-json))]
+                 ::invalid-json)
+               (catch IllegalArgumentException _
+                 ::body-too-large))]
     (cond
       (= body ::body-too-large)
-      {:status 413                                          ;;Content Too Large
+      {:status 413
        :headers {"Content-Type" "application/json"}
        :body (json/generate-string {:error "Request body too large."})}
       (= body ::invalid-json)
-      {:status 400                                          ;;Bad Request
+      {:status 400
        :headers {"Content-Type" "application/json"}
        :body (json/generate-string {:error "Invalid JSON."})}
       :else
-    (let [url (:url body)]
-      (if (or (str/blank? url) (not (valid-url? url)))
-        {:status 400                                        ;;Bad Request
-         :headers {"Content-Type" "application/json"}
-         :body (json/generate-string {:error "No Valid URL found."})}
-      (let [result (shorten-url! ds url)]
-        {:status 201                                        ;;Created
-         :headers {"Content-Type" "application/json"}
-         :body (json/generate-string result)}))))))
+      (let [url (:url body)]
+        (if (or (str/blank? url)
+                (not (valid-url? url)))
+          {:status 400
+           :headers {"Content-Type" "application/json"}
+           :body (json/generate-string {:error "No Valid URL found."})}
+          (try
+            (let [result (shorten-url! ds url)]
+              {:status 201
+               :headers {"Content-Type" "application/json"}
+               :body (json/generate-string result)})
+            (catch RuntimeException _
+              {:status 500
+               :headers {"Content-Type" "application/json"}
+               :body (json/generate-string {:error "Unable to generate unique short code."})})))))))
 
 (defn redirect-handler [ds request]
   (let [code (get-in request [:path-params :code])
         link (db/find-by-code ds code)]
     (if link
-      {:status 302                                          ;;Found
+      {:status 302
        :headers {"Location" (:original_url link)}}
-      {:status 404                                          ;;Not Found
+      {:status 404
        :headers {"Content-Type" "text/plain"}
        :body "Short link not found"})))
 
 (defn html-shorten-handler [ds request]
   (let [body (:form-params request)
         url (get body "url")]
-    (if (or (str/blank? url) (not(valid-url? url)))
-      {:status 400                                          ;;Bad Request
+    (if (or (str/blank? url)
+            (not (valid-url? url)))
+      {:status 400
        :headers {"Content-Type" "text/html"}
        :body "<p>Invalid URL</p>"}
       (let [result (shorten-url! ds url)]
-        {:status 200                                        ;;OK
+        {:status 200
          :headers {"Content-Type" "text/html"}
-         :body (str "<a href=\"" (:short-url result) "\">" (:short-url result) "</a>")}))))
+         :body (str (h/html
+                      [:a {:href (:short-url result)}
+                       (:short-url result)]))}))))
